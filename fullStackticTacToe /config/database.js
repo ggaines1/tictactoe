60430c1bf0f1b8a581c1d62e8306409a4bbe1773
ggaines1 <43713089+ggaines1@users.noreply.github.com>
2018-11-19 00:44:27 -0500

    Add files via upload

---
+++ fullStackticTacToe /config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+// exports an object with a link to database
+module.exports = {
+
+    'url' : 'mongodb://natsu:dragon1@ds161856.mlab.com:61856/anime' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
---
+++ fullStackticTacToe /public/main.js
@@ -0,0 +1,45 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("clear");
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        var speak = new SpeechSynthesisUtterance(`Drinks at the bar fo ${name}`);
+        window.speechSynthesis.speak(speak);
+        fetch('profile', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messagesDel', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
